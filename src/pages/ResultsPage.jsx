a7e8266772b9d88689e39dd74458442a9f48627c
Leonardo Kramer Nadal <leokn0102@gmail.com>
2024-06-07 21:04:41 -0300

    Creating the results page

---
+++ src/pages/ResultsPage.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StyledMainContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  background-image: url("images/imagem-pagina-inicial.png");
+  background-size: contain;
+`;
+
+const StyledContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 2rem;
+  background-color: #ffffff;
+  border-radius: 0 0 100px 100px;
+`;
+
+const StyledTitle = styled.h1`
+  text-align: center;
+  font-size: 30px;
+  color: #5f5f5f;
+  margin-top: 0.5rem;
+`;
+
+const StyledResultParagraph = styled.p`
+  font-size: 35px;
+  margin: 1.5rem 0 0.5rem 0;
+  text-align: center;
+  color: #0b3d91;
+`;
+
+const StyledScoreParagraph = styled.p`
+  font-size: 25px;
+  color: #5f5f5f;
+  text-align: center;
+  margin: 0 0 3rem 0;
+`;
+
+const StyledSpan = styled.span`
+  text-align: center;
+  font-size: 18px;
+  color: #5f5f5f;
+  margin-top: 2rem;
+`;
+
+const ResultsPage = () => {
+  const handleClick = () => {
+    console.log("Olá mundo");
+  };
+
+  return (
+    <StyledMainContainer>
+      <StyledContentBox>
+        <StyledTitle>A qualidade do seu sono é:</StyledTitle>
+        <StyledResultParagraph>
+          Inadequada e insuficiente.
+        </StyledResultParagraph>
+        <StyledScoreParagraph>Seus pontos: 15</StyledScoreParagraph>
+        <PrimaryButton
+          text={"Veja como você pode melhorar o seu sono."}
+          onAction={handleClick}
+        />
+        <StyledSpan>Turma 221 GVB</StyledSpan>
+      </StyledContentBox>
+    </StyledMainContainer>
+  );
+};
+
+export default ResultsPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import GlobalStyle from "./components/GlobalStyles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FirstPage from "./pages/FirstPage";
 import QuestionOne from "./pages/QuestionOne";
+import ResultsPage from "./pages/ResultsPage";
 
 const AppPages = () => {
     return (
@@ -11,6 +12,7 @@ const AppPages = () => {
         <Routes>
             <Route path="/" element={<FirstPage />}/>
             <Route path="/questionOne" element={<QuestionOne />}/>
+            <Route path="/results" element={<ResultsPage />}/>
         </Routes>
     </BrowserRouter>
 );
